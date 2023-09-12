775b8e0c6d021b497f01b928f2a022c0dfba637b
Duarte OC <me@duarteocarmo.com>
2023-09-12 12:37:46 +0200

    footer

--- src/components/FooterView.tsx
+++ src/components/FooterView.tsx
@@ -3,14 +3,25 @@ import Link from "next/link";
 function Footer() {
   return (
     <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center text-gray-500 mt-14">
-      <label className="mb-2 text-gray-900 pr-3">
-        Powered by{" "}
+      <label className="mb-2 text-gray-900 pr-3 text-sm">
+        By{" "}
+        <a href="https://duarteocarmo.com" target="_blank">
+          Duarte O.Carmo
+        </a>{" "}
+        | Powered by{" "}
         <a
           href="https://www.superduperdb.com/?ref=changelog"
           className="text-superduper-purple font-bold"
           target="_blank"
         >
           SuperDuperDB
+        </a>{" "}
+        |{" "}
+        <a
+          href="https://github.com/duarteocarmo/thechangelogbot-frontend"
+          target="_blank"
+        >
+          GitHub
         </a>
       </label>
     </div>
