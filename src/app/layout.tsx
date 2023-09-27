47c86d0322934007f4438680427e6c91059837b2
Duarte OC <me@duarteocarmo.com>
2023-09-27 08:58:06 +0200

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,8 @@ const roboto_mono = Roboto_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Changelog Neural Search",
-  description: "Use neural search to search for podcasts on the Changelog Network",
+  description:
+    "Use neural search to search for podcasts on the Changelog Network",
 };
 
 export default function RootLayout({
@@ -16,6 +17,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          defer
+          data-domain="changelog.duarteocarmo.com"
+          src="https://plausible.io/js/script.js"
+        ></script>
+      </head>
       <body className={roboto_mono.className}>{children}</body>
     </html>
   );
